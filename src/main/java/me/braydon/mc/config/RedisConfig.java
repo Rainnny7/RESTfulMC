59efbe437da6dd12234745c8664907e80bb44949
Rainnny7 <braydonrainnny@gmail.com>
2024-04-08 01:47:23 -0400

    Move Redis to its own config

--- src/main/java/me/braydon/mc/config/RedisConfig.java
+++ src/main/java/me/braydon/mc/config/RedisConfig.java
@@ -674,46 +674,72 @@
  * Public License instead of this License.  But first, please read
  * <https://www.gnu.org/licenses/why-not-lgpl.html>.
  */
-package me.braydon.mc;
+package me.braydon.mc.config;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import lombok.NonNull;
-import lombok.SneakyThrows;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
-
-import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
-import java.util.Objects;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
 /**
  * @author Braydon
  */
-@SpringBootApplication(exclude = { JacksonAutoConfiguration.class })
-@Slf4j(topic = "RESTfulMC")
-public class RESTfulMC {
-    public static final Gson GSON = new GsonBuilder()
-            .setDateFormat("MM-dd-yyyy HH:mm:ss")
-            .create();
+@Configuration
+public class RedisConfig {
+    /**
+     * The Redis server host.
+     */
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
 
-    @SneakyThrows
-    public static void main(@NonNull String[] args) {
-        // Handle loading of our configuration file
-        File config = new File("application.yml");
-        if (!config.exists()) { // Saving the default config if it doesn't exist locally
-            Files.copy(Objects.requireNonNull(RESTfulMC.class.getResourceAsStream("/application.yml")), config.toPath(), StandardCopyOption.REPLACE_EXISTING);
-            log.info("Saved the default configuration to '{}', please re-launch the application", // Log the default config being saved
-                    config.getAbsolutePath()
-            );
-            return;
+    /**
+     * The Redis server port.
+     */
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    /**
+     * The Redis database index.
+     */
+    @Value("${spring.data.redis.database}")
+    private int redisDatabase;
+
+    /**
+     * The optional Redis password.
+     */
+    @Value("${spring.data.redis.auth}")
+    private String redisAuth;
+
+    /**
+     * Build the config to use for Redis.
+     *
+     * @return the config
+     * @see RedisTemplate for config
+     */
+    @Bean @NonNull
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        return template;
+    }
+
+    /**
+     * Build the connection factory to use
+     * when making connections to Redis.
+     *
+     * @return the built factory
+     * @see JedisConnectionFactory for factory
+     */
+    @Bean @NonNull
+    public JedisConnectionFactory jedisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisHost, redisPort);
+        config.setDatabase(redisDatabase);
+        if (!redisAuth.trim().isEmpty()) { // Auth with our provided password
+            config.setPassword(redisAuth);
         }
-        log.info("Found configuration at '{}'", config.getAbsolutePath()); // Log the found config
-
-        // Start the app
-        SpringApplication.run(RESTfulMC.class, args);
+        return new JedisConnectionFactory(config);
     }
 }
