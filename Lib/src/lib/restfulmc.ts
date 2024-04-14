abccde5896a4a2ced40bd755f5a608c807d823e7
Rainnny7 <braydonrainnny@gmail.com>
2024-04-14 14:35:55 -0400

    Update Player type

--- Lib/src/lib/restfulmc.ts
+++ Lib/src/lib/restfulmc.ts
@@ -1,3 +1,4 @@
+import { ErrorResponse } from "../types/generic";
 import type { Player } from "../types/player";
 
 const ENDPOINT = "https://mc.rainnny.club"; // The API endpoint to use
@@ -11,6 +12,13 @@ const ENDPOINT = "https://mc.rainnny.club"; // The API endpoint to use
 export const getPlayer = (query: string): Promise<Player> => {
 	return new Promise(async (resolve, reject) => {
 		const response: Response = await fetch(`${ENDPOINT}/player/${query}`); // Request the player
-		resolve((await response.json()) as Player); // Resolve the player
+		const json: any = await response.json();
+
+		// Resolve the player
+		if (response.ok) {
+			resolve(json as Player);
+		} else {
+			reject(json as ErrorResponse); // The request failed
+		}
 	});
 };
--- Lib/src/types/player.d.ts
+++ Lib/src/types/player.d.ts
@@ -13,7 +13,32 @@ export type Player = {
 	 * The skin of this player.
 	 */
 	skin: Skin;
-}
+
+	/**
+	 * The cape of this player, undefined if none.
+	 */
+	cape?: Cape | undefined;
+
+	/**
+	 * The properties of the Mojang
+	 * profile for this player.
+	 */
+	properties: ProfileProperty[];
+
+	/**
+	 * The profile actions this player has, undefined if none.
+	 */
+	profileActions?: ProfileAction[] | undefined;
+
+	/**
+	 * Is this player legacy?
+	 * <p>
+	 * A "Legacy" player is a player that
+	 * has not yet migrated to a Mojang account.
+	 * </p>
+	 */
+	legacy: boolean;
+};
 
 /**
  * A skin for a {@link Player}.
@@ -27,15 +52,62 @@ type Skin = {
 	/**
 	 * The model of this skin.
 	 */
-	model: Model;
+	model: SkinModel;
 
 	/**
 	 * Is this skin legacy?
 	 */
 	legacy: boolean;
-}
+
+	/**
+	 * URLs to the parts of this skin.
+	 * <p>
+	 * The key is the part name, and
+	 * the value is the URL.
+	 * </p>
+	 */
+	parts: {
+		[key: string]: string;
+	};
+};
 
 /**
  * Possible models for a skin.
  */
-type Model = "default" | "slim";
+type SkinModel = "default" | "slim";
+
+/**
+ * A cape for a {@link Player}.
+ */
+type Cape = {
+	/**
+	 * The texture URL of this cape.
+	 */
+	url: string;
+};
+
+/**
+ * A property of a Mojang profile.
+ */
+type ProfileProperty = {
+	/**
+	 * The name of this property.
+	 */
+	name: string;
+
+	/**
+	 * The Base64 value of this property.
+	 */
+	value: string;
+
+	/**
+	 * The Base64 signature of this
+	 * property, undefined if not signed.
+	 */
+	signature?: string | undefined;
+};
+
+/**
+ * Profile actions that can
+ */
+type ProfileAction = "FORCED_NAME_CHANGE" | "USING_BANNED_SKIN";
