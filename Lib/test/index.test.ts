acae5dbcd6ee10e3169af4831c96dc8fa9f34fb8
Rainnny7 <braydonrainnny@gmail.com>
2024-04-14 16:35:55 -0400

    Disable tests, not done yet

--- Lib/test/index.test.ts
+++ Lib/test/index.test.ts
@@ -1,30 +1,24 @@
-import { describe, it } from "bun:test";
-import { getMinecraftServer, getMojangServerStatus, getPlayer } from "../src";
-import { CachedPlayer } from "../src/types/player";
-import { BedrockMinecraftServer } from "../src/types/server/bedrock-server";
-import { JavaMinecraftServer } from "../src/types/server/java-server";
+// describe("player", () => {
+// 	it("Rainnny", async () => {
+// 		const player: CachedPlayer = await getPlayer("Rainnny");
+// 		console.log("player found", player.cached);
+// 	});
+// });
 
-describe("player", () => {
-	it("Rainnny", async () => {
-		const player: CachedPlayer = await getPlayer("Rainnny");
-		console.log("player found", player.cached);
-	});
-});
+// describe("server", () => {
+// 	it("java", async () => {
+// 		const server: JavaMinecraftServer | BedrockMinecraftServer =
+// 			await getMinecraftServer("java", "play.wildnetwork.net");
 
-describe("server", () => {
-	it("java", async () => {
-		const server: JavaMinecraftServer | BedrockMinecraftServer =
-			await getMinecraftServer("java", "play.wildnetwork.net");
+// 		if ((server as BedrockMinecraftServer).id) {
+// 		}
+// 		console.log(server.ip);
+// 	});
+// });
 
-		if ((server as BedrockMinecraftServer).id) {
-		}
-		console.log(server.ip);
-	});
-});
-
-describe("mojang", () => {
-	it("status", async () => {
-		const status: MojangServerStatus = await getMojangServerStatus();
-		console.log(status["https://sessionserver.mojang.com"]);
-	});
-});
+// describe("mojang", () => {
+// 	it("status", async () => {
+// 		const status: MojangServerStatus = await getMojangServerStatus();
+// 		console.log(status["https://sessionserver.mojang.com"]);
+// 	});
+// });
