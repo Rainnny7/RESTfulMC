a51528616d67522ccae3b1726c94764208b8084f
Rainnny7 <braydonrainnny@gmail.com>
2024-04-15 10:32:44 -0400

    Fix the build not including all types

---
+++ JS-SDK/src/types/error.d.ts
@@ -0,0 +1,24 @@
+/**
+ * A response representing an error.
+ */
+export type RestfulMCAPIError = {
+	/**
+	 * The status of this error.
+	 */
+	status: string;
+
+	/**
+	 * The HTTP code of this error.
+	 */
+	code: number;
+
+	/**
+	 * The message of this error.
+	 */
+	message: string;
+
+	/**
+	 * The timestamp this error occurred.
+	 */
+	timestamp: string;
+}
