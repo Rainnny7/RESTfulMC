bb81be1e28d5ca6f3ee3a6f1310f74db463c31c9
Rainnny7 <braydonrainnny@gmail.com>
2024-04-15 10:38:35 -0400

    Fix tests not running

---
+++ JS-SDK/src/index.d.ts
@@ -0,0 +1,5 @@
+export * from "./types/mojang";
+export * from "./types/player";
+export * from "./types/server/bedrock-server";
+export * from "./types/server/java-server";
+export * from "./types/server/server";
--- JS-SDK/src/index.ts
+++ JS-SDK/src/index.ts
@@ -1,7 +1,2 @@
 export * from "@/lib/restfulmc";
 export * from "@/types/error";
-export * from "@/types/mojang";
-export * from "@/types/player";
-export * from "@/types/server/bedrock-server";
-export * from "@/types/server/java-server";
-export * from "@/types/server/server";
