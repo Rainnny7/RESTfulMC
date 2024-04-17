69ce0fc469f41241dee2c2bf20c96c9cd066d56d
Rainnny7 <braydonrainnny@gmail.com>
2024-04-17 19:19:52 -0400

    Update the docs page

--- Frontend/src/app/(pages)/docs/page.tsx
+++ Frontend/src/app/(pages)/docs/page.tsx
@@ -18,24 +18,22 @@ export const metadata: Metadata = {
  * @returns the page jsx
  */
 const DocsPage = (): ReactElement => (
-    <main className="h-[64vh] flex flex-col gap-3 justify-center items-center">
+    <main className="h-[84vh] flex flex-col gap-3 justify-center items-center text-center pointer-events-none">
         {/* Creeper */}
-        <div className="absolute left-28 bottom-16 pointer-events-none">
-            <Creeper />
-        </div>
+        <Creeper />
 
         {/* Header */}
         <h1
             className={cn(
-                "text-6xl text-minecraft-green-3 pointer-events-none",
+                "text-5xl sm:text-6xl text-minecraft-green-3",
                 minecrafter.className
             )}
         >
             Documentation
         </h1>
 
-        {/* Content */}
-        <h2 className="text-xl">
+        {/* Error */}
+        <h2 className="text-2xl">
             This page is still under construction, however we do have a{" "}
             <Link
                 className="text-minecraft-green-4"
