ae4ece4ee5dd7ddb829335eb37d7d48c80d181c2
Rainnny7 <braydonrainnny@gmail.com>
2024-04-16 23:28:12 -0400

    does this work?

--- Frontend/src/app/(pages)/player/[[...slug]]/page.tsx
+++ Frontend/src/app/(pages)/player/[[...slug]]/page.tsx
@@ -79,7 +79,7 @@ export const generateMetadata = async ({
 		try {
 			const player: CachedPlayer = await getPlayer(query); // Get the player to embed
 			return Embed({
-				title: `${player.username}'s Profile${player.legacy && " (Legacy)"}`,
+				title: `${player.username}'s Profile`,
 				description: `UUID: ${player.uniqueId}\n\nClick to view data about this player.`,
 				thumbnail: player.skin.parts.HEAD,
 			});
