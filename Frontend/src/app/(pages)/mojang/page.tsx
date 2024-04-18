a57e95806e7fd6a45a287a8263cd271decd14373
Rainnny7 <braydonrainnny@gmail.com>
2024-04-18 14:04:54 -0400

    Fix card spacing

--- Frontend/src/app/(pages)/mojang/page.tsx
+++ Frontend/src/app/(pages)/mojang/page.tsx
@@ -45,13 +45,16 @@ const MojangStatusPage = async (): Promise<ReactElement> => {
             </h1>
 
             {/* Server Statuses */}
-            <div className="w-[25rem] xs:w-[29rem] sm:w-[33.5rem] px-5 py-2 flex flex-col gap-2.5 bg-muted rounded-xl divide-y-2 divide-zinc-700/40 transition-all transform-gpu">
+            <div className="w-[25rem] xs:w-[29rem] sm:w-[33.5rem] px-5 py-3.5 flex flex-col gap-2.5 bg-muted rounded-xl divide-y-2 divide-zinc-700/40 transition-all transform-gpu">
                 {servers.map((server, index) => {
                     const status: MojangServerStatus = server.status; // The status of the server
                     return (
                         <div
                             key={index}
-                            className="pt-2 flex justify-between items-center"
+                            className={cn(
+                                "flex justify-between items-center",
+                                index > 0 && "pt-2"
+                            )}
                         >
                             <div className="flex flex-col gap-0.5">
                                 <h1 className="pointer-events-none">
