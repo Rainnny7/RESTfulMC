f021eb48094b2a5540edced3af6eaa8595381f13
Rainnny7 <braydonrainnny@gmail.com>
2024-04-22 01:03:52 -0400

    Add simple tooltip component

---
+++ Frontend/src/app/components/simple-tooltip.tsx
@@ -0,0 +1,39 @@
+import { ReactElement, ReactNode } from "react";
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+/**
+ * The props for a simple tooltip.
+ */
+type SimpleTooltipProps = {
+    /**
+     * The content to display in the tooltip.
+     */
+    content: string;
+
+    /**
+     * The children to render in this tooltip.
+     */
+    children: ReactNode;
+};
+
+/**
+ * A simple tooltip, this is wrapping the
+ * shadcn tooltip to make it easier to use.
+ *
+ * @return the tooltip jsx
+ */
+const SimpleTooltip = ({
+    content,
+    children,
+}: SimpleTooltipProps): ReactElement => (
+    <Tooltip>
+        <TooltipTrigger asChild>{children}</TooltipTrigger>
+        <TooltipContent>{content}</TooltipContent>
+    </Tooltip>
+);
+
+export default SimpleTooltip;
