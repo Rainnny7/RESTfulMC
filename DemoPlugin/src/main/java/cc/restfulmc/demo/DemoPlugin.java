00a9dc0ce8823e83b332b6d4c8893c44366ee69f
Rainnny7 <braydonrainnny@gmail.com>
2024-04-26 15:13:09 -0400

    Prevent logins

--- DemoPlugin/src/main/java/cc/restfulmc/demo/DemoPlugin.java
+++ DemoPlugin/src/main/java/cc/restfulmc/demo/DemoPlugin.java
@@ -2,10 +2,13 @@ package cc.restfulmc.demo;
 
 import cc.restfulmc.demo.listener.ServerPingListener;
 import com.google.inject.Inject;
+import com.velocitypowered.api.event.PostOrder;
 import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.connection.PreLoginEvent;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.proxy.ProxyServer;
+import net.kyori.adventure.text.Component;
 
 /**
  * @author Braydon
@@ -22,5 +25,10 @@ public final class DemoPlugin {
     @Subscribe
     public void onProxyInitialize(ProxyInitializeEvent event) {
         server.getEventManager().register(this, new ServerPingListener());
+    }
+
+    @Subscribe(order = PostOrder.FIRST)
+    public void onLogin(PreLoginEvent event) {
+        event.setResult(PreLoginEvent.PreLoginComponentResult.denied(Component.text("§cYou can't join a demo server :(")));
     }
 }
