0ec514eb238f26d3b05b10d7197811e2a6b58d42
Braydon <braydonrainnny@gmail.com>
2026-02-05 01:19:57 -0500

    test

--- www/src/app/opengraph-image.tsx
+++ www/src/app/opengraph-image.tsx
@@ -1,4 +1,3 @@
-import { env } from "@/lib/env";
 import {
     OG_IMAGE_HEIGHT,
     OG_IMAGE_WIDTH,
@@ -6,18 +5,19 @@ import {
 } from "@/lib/og-image";
 import { ImageResponse } from "next/og";
 
-export const alt = "RESTfulMC – A simple, yet useful RESTful API for Minecraft";
+export const alt =
+    "RESTfulMC - A powerful RESTful API for Minecraft utilizing Spring Boot.";
 export const size = { width: OG_IMAGE_WIDTH, height: OG_IMAGE_HEIGHT };
 export const contentType = "image/png";
 
-const RootOgImage = () => {
+const PagesOgImage = () => {
     return new ImageResponse(
         <OgImageTemplate
             title="A powerful RESTful API for Minecraft"
             description="Simple, useful REST API for Minecraft utilizing Spring Boot."
-            imageUrl={`${env.NEXT_PUBLIC_BASE_URL}/media/landing.webp`}
+            imageUrl={`${process.env.NEXT_PUBLIC_BASE_URL}/media/landing.webp`}
         />,
         { width: OG_IMAGE_WIDTH, height: OG_IMAGE_HEIGHT }
     );
 };
-export default RootOgImage;
+export default PagesOgImage;
