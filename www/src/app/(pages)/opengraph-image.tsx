6f9ddadf090a4e0f9efc0014eb63d2cf6e2b3694
Braydon <braydonrainnny@gmail.com>
2026-02-05 01:05:54 -0500

    missng .env.example

--- www/src/app/(pages)/opengraph-image.tsx
+++ www/src/app/(pages)/opengraph-image.tsx
@@ -1,8 +1,8 @@
 import { env } from "@/lib/env";
 import {
-    OgImageTemplate,
     OG_IMAGE_HEIGHT,
     OG_IMAGE_WIDTH,
+    OgImageTemplate,
 } from "@/lib/og-image";
 import { ImageResponse } from "next/og";
 
@@ -12,18 +12,14 @@ export const size = { width: OG_IMAGE_WIDTH, height: OG_IMAGE_HEIGHT };
 export const contentType = "image/png";
 
 const PagesOgImage = () => {
-    const baseUrl = env.NEXT_PUBLIC_BASE_URL ?? "http://localhost:3000";
     return new ImageResponse(
-        (
-            <OgImageTemplate
-                title="A powerful RESTful API for Minecraft"
-                description="Simple, useful REST API for Minecraft utilizing Spring Boot."
-                siteName="RESTfulMC"
-                imageUrl={`${baseUrl}/media/landing.webp`}
-            />
-        ),
+        <OgImageTemplate
+            title="A powerful RESTful API for Minecraft"
+            description="Simple, useful REST API for Minecraft utilizing Spring Boot."
+            siteName="RESTfulMC"
+            imageUrl={`${env.NEXT_PUBLIC_BASE_URL}/media/landing.webp`}
+        />,
         { width: OG_IMAGE_WIDTH, height: OG_IMAGE_HEIGHT }
     );
 };
-
 export default PagesOgImage;
--- www/src/app/opengraph-image.tsx
+++ www/src/app/opengraph-image.tsx
@@ -1,8 +1,8 @@
 import { env } from "@/lib/env";
 import {
-    OgImageTemplate,
     OG_IMAGE_HEIGHT,
     OG_IMAGE_WIDTH,
+    OgImageTemplate,
 } from "@/lib/og-image";
 import { ImageResponse } from "next/og";
 
@@ -11,18 +11,14 @@ export const size = { width: OG_IMAGE_WIDTH, height: OG_IMAGE_HEIGHT };
 export const contentType = "image/png";
 
 const RootOgImage = () => {
-    const baseUrl = env.NEXT_PUBLIC_BASE_URL ?? "http://localhost:3000";
     return new ImageResponse(
-        (
-            <OgImageTemplate
-                title="RESTfulMC"
-                description="A simple, yet useful RESTful API for Minecraft utilizing Spring Boot."
-                siteName="RESTfulMC"
-                imageUrl={`${baseUrl}/media/landing.webp`}
-            />
-        ),
+        <OgImageTemplate
+            title="RESTfulMC"
+            description="A simple, yet useful RESTful API for Minecraft utilizing Spring Boot."
+            siteName="RESTfulMC"
+            imageUrl={`${env.NEXT_PUBLIC_BASE_URL}/media/landing.webp`}
+        />,
         { width: OG_IMAGE_WIDTH, height: OG_IMAGE_HEIGHT }
     );
 };
-
 export default RootOgImage;
