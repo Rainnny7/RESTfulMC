a70fefe5d0bf305eaf567016040d0c15b555ff32
Braydon <braydonrainnny@gmail.com>
2026-02-06 19:32:54 -0500

    fix card border colors not being the same

--- www/src/components/ui/card.tsx
+++ www/src/components/ui/card.tsx
@@ -25,7 +25,7 @@ function CardHeader({ className, ...props }: React.ComponentProps<"div">) {
         <div
             data-slot="card-header"
             className={cn(
-                "bg-muted/50 border-b gap-1 rounded-t-xl px-3 py-1.5 group-data-[size=sm]/card:px-3 group-data-[size=sm]/card:[.border-b]:pb-3 group/card-header @container/card-header grid auto-rows-min items-start has-data-[slot=card-action]:grid-cols-[1fr_auto] has-data-[slot=card-description]:grid-rows-[auto_auto]",
+                "bg-muted/50 border-b border-border gap-1 rounded-t-xl px-3 py-1.5 group-data-[size=sm]/card:px-3 group-data-[size=sm]/card:[.border-b]:pb-3 group/card-header @container/card-header grid auto-rows-min items-start has-data-[slot=card-action]:grid-cols-[1fr_auto] has-data-[slot=card-description]:grid-rows-[auto_auto]",
                 className
             )}
             {...props}
@@ -84,7 +84,7 @@ function CardFooter({ className, ...props }: React.ComponentProps<"div">) {
         <div
             data-slot="card-footer"
             className={cn(
-                "bg-muted/50 rounded-b-xl border-t p-4 group-data-[size=sm]/card:p-3 flex items-center",
+                "bg-muted/50 rounded-b-xl border-t border-border p-4 group-data-[size=sm]/card:p-3 flex items-center",
                 className
             )}
             {...props}
